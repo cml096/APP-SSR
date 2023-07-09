f8802b34105a68d20dcb67d77a50ba7b38894f43
Cristobal <cristobalmontanolopez@gmail.com>
2023-07-09 19:32:17 -0400

    Link and proxy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ['images-assets.nasa.gov']
-    }
+    },
+    async rewrites () {
+        return [
+            {
+                source: '/galaxia/:path*',
+                destination: '/galaxy/:path*',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
